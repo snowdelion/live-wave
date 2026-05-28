086cd88e0b14b0a087b2a335453c6d1a8f2421ae
snowfall <ammm101046@gmail.com>
2026-05-28 23:10:44 +0300

    chore(api/eslint): disable no-unsafe-argument for testing files
    - turn off the rule globally for `*.spec` and `*.test` files

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -68,6 +68,7 @@ export default [
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 
