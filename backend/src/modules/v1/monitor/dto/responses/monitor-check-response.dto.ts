119c6a27364a3372dcf78db915a53288c9d01d5d
snowfall <ammm101046@gmail.com>
2026-06-11 19:07:25 +0300

    fix(api/v1/monitor): specify proper HttpCheckDetails type for details field

--- backend/src/modules/v1/monitor/dto/responses/monitor-check-response.dto.ts
+++ backend/src/modules/v1/monitor/dto/responses/monitor-check-response.dto.ts
@@ -1,40 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { Method, RecordType, StatusEnum } from '@prisma/client'
 
-export class MonitorCheckResponseDto {
-  @ApiProperty({ example: 'ck3kq1t7x0000u1w6p8x1f1c2' })
-  id!: string
-
-  @ApiProperty({ example: 'up', enum: StatusEnum })
-  status!: StatusEnum
-
-  @ApiProperty({ example: 200, nullable: true })
-  statusCode!: number | null
-
-  @ApiProperty({ example: 1000, nullable: true })
-  responseTime!: number | null
-
-  @ApiProperty({ example: null, nullable: true })
-  error!: string | null
-
-  @ApiPropertyOptional({
-    oneOf: [
-      { $ref: '#/components/schemas/HttpCheckDetails' },
-      { $ref: '#/components/schemas/TcpCheckDetails' },
-      { $ref: '#/components/schemas/IcmpCheckDetails' },
-      { $ref: '#/components/schemas/DnsCheckDetails' },
-    ],
-    description: 'Check details depending on monitor type',
-  })
-  details?: HttpCheckDetails | TcpCheckDetails | IcmpCheckDetails | DnsCheckDetails
-
-  @ApiProperty({ example: 'cmpplwrap0000u1cwddpe8mq8' })
-  monitorId!: string
-
-  @ApiProperty({ example: '2026-05-28T17:16:12.045Z', format: 'date-time' })
-  checkedAt!: Date
-}
-
 export class HttpCheckDetails {
   @ApiProperty({ example: 'https://example.com' })
   url!: string
@@ -62,4 +28,39 @@ export class DnsCheckDetails {
 
   @ApiProperty({ example: RecordType.AAAA })
   recordType!: RecordType
+}
+
+export class MonitorCheckResponseDto {
+  @ApiProperty({ example: 'ck3kq1t7x0000u1w6p8x1f1c2' })
+  id!: string
+
+  @ApiProperty({ example: 'up', enum: StatusEnum })
+  status!: StatusEnum
+
+  @ApiProperty({ example: 200, nullable: true })
+  statusCode!: number | null
+
+  @ApiProperty({ example: 1000, nullable: true })
+  responseTime!: number | null
+
+  @ApiProperty({ example: null, nullable: true })
+  error!: string | null
+
+  @ApiPropertyOptional({
+    oneOf: [
+      { $ref: '#/components/schemas/HttpCheckDetails' },
+      { $ref: '#/components/schemas/TcpCheckDetails' },
+      { $ref: '#/components/schemas/IcmpCheckDetails' },
+      { $ref: '#/components/schemas/DnsCheckDetails' },
+    ],
+    description: 'Check details depending on monitor type',
+    type: HttpCheckDetails,
+  })
+  details?: HttpCheckDetails | TcpCheckDetails | IcmpCheckDetails | DnsCheckDetails
+
+  @ApiProperty({ example: 'cmpplwrap0000u1cwddpe8mq8' })
+  monitorId!: string
+
+  @ApiProperty({ example: '2026-05-28T17:16:12.045Z', format: 'date-time' })
+  checkedAt!: Date
 }
