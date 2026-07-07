703b64e6e7f7d16161a116cb552694017b915bb4
snowfall <ammm101046@gmail.com>
2026-07-07 22:04:37 +0300

    feat(users): add GET v1/users/me endpoint to get user info
    - protect wth `AuthGuard`; implement docs
    - return basic info and monitors/checks counters
    - format value into the only object without nesting

--- backend/src/modules/v1/users/users.docs.ts
+++ backend/src/modules/v1/users/users.docs.ts
@@ -1,8 +1,17 @@
 import { HttpStatus } from '@nestjs/common'
 
+import { GetMeResponseDto } from './dto/get-me-response.dto'
+
 export const deleteDocs = {
   summary: 'Completely deletes account',
   description: 'Permanently deletes the user account and all associated data',
   extraResponses: [{ status: HttpStatus.NO_CONTENT }],
+  isProtected: true,
+}
+
+export const getMeDocs = {
+  summary: 'Gets current user base info',
+  description: 'Returns base info, count of monitors, settings (enabled/disabled notifications)',
+  extraResponses: [{ status: HttpStatus.OK, type: GetMeResponseDto }],
   isProtected: true,
 }
