2809801f8ab13a961cf00de72ad10cb60ebf52bc
snowdelion <ammm101046@gmail.com>
2026-07-13 13:15:31 +0300

    refactor(auth): limit confirmPassword to 16 characters (#41)
    - add `.max(16)` to `confirmPassword` in `registerSchema`

--- frontend/src/pages-flat/auth/model/auth.schema.ts
+++ frontend/src/pages-flat/auth/model/auth.schema.ts
@@ -5,7 +5,10 @@ import { AuthViaEmailRequestSchema } from '@/entities/auth'
 export const loginSchema = AuthViaEmailRequestSchema
 
 export const registerSchema = AuthViaEmailRequestSchema.extend({
-  confirmPassword: z.string().min(8, 'Confirm password must be at least 8 characters'),
+  confirmPassword: z
+    .string()
+    .min(8, 'Confirm password should be between 8-16 characters')
+    .max(16, 'Confirm password should be between 8-16 characters'),
 }).refine(data => data.password === data.confirmPassword, {
   message: "Passwords don't match",
   path: ['confirmPassword'],
